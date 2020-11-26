9ef22125279749697a9482df9f1cdff89b1fab0b
sitgofour <evansmithcodes@gmail.com>
2020-11-25 20:56:38 -0800

    modified bot

--- index.js
+++ index.js
@@ -20,31 +20,44 @@ const rl = readline.createInterface({
 
 console.log("hi there, I'm node bot!");
 
-rl.question("what's your favorite fruit?\n", (userResponse) => {
-    const fruits = {
-        "apples": ["i love apples too", "i like some apples, but not green ones", "i like them too, apple pie is my favorite"],
-        "oranges": ["i don't really like oranges", "nothing better than an orange", "orange juice rocks my socks!"],
-        "bananas": ["bananas are yum yum", "they great unless they get mushy", "banana sundae please"],
-        "not found": ["are you sure that's a fruit, i've never heard of it", "Is that a real fruit??", "Is that really a fruit, sure you spelled it right?"]
+rl.on('line', (input) => {
+    if(input === "fruit") {
+        engageFruit();
+    } else {
+        console.log("i dont understand that input");
     }
+})
 
-    try {
-        if(fruits[userResponse]) {
-            let randIndex = Math.floor(Math.random() * fruits[userResponse].length);
-            console.log(fruits[userResponse][randIndex]);
-        } else {
-            let randIndex = Math.floor(Math.random() * fruits["not found"].length);
-            console.log(fruits["not found"][randIndex]);
+
+function engageFruit() {
+    rl.question("what's your favorite fruit?\n", (userResponse) => {
+        const fruits = {
+            "apples": ["i love apples too", "i like some apples, but not green ones", "i like them too, apple pie is my favorite"],
+            "oranges": ["i don't really like oranges", "nothing better than an orange", "orange juice rocks my socks!"],
+            "bananas": ["bananas are yum yum", "they great unless they get mushy", "banana sundae please"],
+            "not found": ["are you sure that's a fruit, i've never heard of it", "Is that a real fruit??", "Is that really a fruit, sure you spelled it right?"]
         }
-    }
-    catch(e) {
-        console.log(e);
-    }
-    finally {
-        rl.close();
-        process.exit(0);
-    }
+    
+        try {
+            if(fruits[userResponse]) {
+                let randIndex = Math.floor(Math.random() * fruits[userResponse].length);
+                console.log(fruits[userResponse][randIndex]);
+            } else {
+                let randIndex = Math.floor(Math.random() * fruits["not found"].length);
+                console.log(fruits["not found"][randIndex]);
+            }
+        }
+        catch(e) {
+            console.log(e);
+        }
+        finally {
+            rl.close();
+            process.exit(0);
+        }
+    
+    });
+}
 
-});
+
 
 
